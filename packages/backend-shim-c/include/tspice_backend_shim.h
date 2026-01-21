7c9628da8609e1acab7afbacac21a355853771b1
CharlieHelps <charlie@charlielabs.ai>
2026-01-21 08:16:55 +0000

    Fix tspice_ktotal_all header prototype

--- packages/backend-shim-c/include/tspice_backend_shim.h
+++ packages/backend-shim-c/include/tspice_backend_shim.h
@@ -13,7 +13,7 @@ int tspice_furnsh(const char *path, char *err, int errMaxBytes);
 
 int tspice_unload(const char *path, char *err, int errMaxBytes);
 
-int tspice_ktotal_all(SpiceInt *outCount, char *err, int errMaxBytes);
+int tspice_ktotal_all(char *err, int errMaxBytes);
 
 #ifdef __cplusplus
 }
